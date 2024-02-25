3277a0dbab246b3a887400c28961a21d3232f105
leo-amaro-conestoga <lamaro3722@conestogac.on.ca>
2024-02-25 01:27:52 -0500

    re-init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,52 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route
+          path="/privacy-policy"
+          element={
+            <Layout>
+              <PrivacyPolicy />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;

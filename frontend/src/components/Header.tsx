a9cdba86d58a7fcbf960011da9ea3b802100fe5f
leo-amaro-conestoga <lamaro3722@conestogac.on.ca>
2024-03-05 14:07:18 -0500

    changed the button to rounded

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
           ) : (
             <Link
               to="/sign-in"
-              className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+              className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100 rounded"
             >
               Sign In
             </Link>

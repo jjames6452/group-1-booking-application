b2fc2feb0678ff37b06817cc0fb3bf2d97fb6d1c
leo-amaro-conestoga <lamaro3722@conestogac.on.ca>
2024-03-09 18:35:35 -0500

    add padding to container

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -27,7 +27,7 @@ const SignIn = () => {
       showToast({ message: "Sign in Successful!", type: "SUCCESS" });
       await queryClient.invalidateQueries("validateToken");
       // navigate(location.state?.from?.pathname || "/");
-      navigate('/')
+      navigate("/");
     },
     onError: (error: Error) => {
       showToast({ message: error.message, type: "ERROR" });
@@ -39,51 +39,53 @@ const SignIn = () => {
   });
 
   return (
-    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
-      <h2 className="text-3xl font-bold">Sign In</h2>
-      <label className="text-gray-700 text-sm font-bold flex-1">
-        Email
-        <input
-          type="email"
-          className="border rounded w-full py-1 px-2 font-normal"
-          {...register("email", { required: "This field is required" })}
-        ></input>
-        {errors.email && (
-          <span className="text-red-500">{errors.email.message}</span>
-        )}
-      </label>
-      <label className="text-gray-700 text-sm font-bold flex-1">
-        Password
-        <input
-          type="password"
-          className="border rounded w-full py-1 px-2 font-normal"
-          {...register("password", {
-            required: "This field is required",
-            minLength: {
-              value: 6,
-              message: "Password must be at least 6 characters",
-            },
-          })}
-        ></input>
-        {errors.password && (
-          <span className="text-red-500">{errors.password.message}</span>
-        )}
-      </label>
-      <span className="flex items-center justify-between">
-        <span className="text-sm">
-          Not Registered?{" "}
-          <Link className="underline" to="/register">
-            Create an account here
-          </Link>
+    <div className="container mx-auto m-10 px-4 lg:w-1/2">
+      <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+        <h2 className="text-3xl font-bold">Sign In</h2>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Email
+          <input
+            type="email"
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("email", { required: "This field is required" })}
+          ></input>
+          {errors.email && (
+            <span className="text-red-500">{errors.email.message}</span>
+          )}
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Password
+          <input
+            type="password"
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("password", {
+              required: "This field is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+          ></input>
+          {errors.password && (
+            <span className="text-red-500">{errors.password.message}</span>
+          )}
+        </label>
+        <span className="flex items-center justify-between">
+          <span className="text-sm">
+            Not Registered?{" "}
+            <Link className="underline" to="/register">
+              Create an account here
+            </Link>
+          </span>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded"
+          >
+            Login
+          </button>
         </span>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded"
-        >
-          Login
-        </button>
-      </span>
-    </form>
+      </form>
+    </div>
   );
 };
 

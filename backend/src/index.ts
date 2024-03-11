218c3355eadb493cc82d6899f371e4be262880a8
leo-amaro-conestoga <lamaro3722@conestogac.on.ca>
2024-03-10 21:33:08 -0400

    Added config to deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
@@ -32,6 +32,9 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 app.listen(7000, () => {
   console.log("server is running");
 });
